34512b0142bc7defd0e9e8f404e7f4772b10ceef
Badal2026 <badalmanishkumar.patel"ontariotechu.net>
2024-10-30 23:49:13 -0400

    made Login and Register both in Auth Folder

--- ResumeBuilder/Controllers/AuthController.cs
+++ ResumeBuilder/Controllers/AuthController.cs
@@ -20,5 +20,23 @@ namespace ResumeBuilder.Controllers
                 return RedirectToAction("Register", "Auth");
             } 
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "admin@admin.com" && password == "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Invalid email or password.";
+                return RedirectToAction("Register", "Auth");
+            }
+        }
+
     }
 }
