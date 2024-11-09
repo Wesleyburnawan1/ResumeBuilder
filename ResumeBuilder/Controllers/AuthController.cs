a9cd27b834ec082c191890c54f955a8420e19e10
wesleyburnawan <wesley.burnawan@gmail.com>
2024-11-09 04:40:51 -0500

    test making test fail

--- ResumeBuilder/Controllers/AuthController.cs
+++ ResumeBuilder/Controllers/AuthController.cs
@@ -57,7 +57,7 @@ namespace ResumeBuilder.Controllers
                 if (existingUser != null)
                 {
                     TempData["ErrorMessage"] = "Email already Registered";
-                    return RedirectToAction("Registerr", "Auth");
+                    return RedirectToAction("Register", "Auth");
                 }
 
                 // Hash the password
