7d79711aeaf51c2ee21180ecff670e87641e5a23
wesleyburnawan <wesley.burnawan@gmail.com>
2024-11-27 14:09:02 -0500

    add start script

---
+++ ResumeBuilder/start-dotnet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install .NET SDK and Runtime for .NET 8.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
+
+# Add .NET to the PATH
+export PATH="$HOME/.dotnet:$PATH"
+
+# Verify installation
+dotnet --version
+
+# Start the application
+dotnet ./publish/ResumeBuilder.dll
