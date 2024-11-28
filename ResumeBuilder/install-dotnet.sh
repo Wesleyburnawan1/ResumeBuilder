c86fe3f6527cac22e102a281df5c78a24510aa97
wesleyburnawan <wesley.burnawan@gmail.com>
2024-11-28 15:05:39 -0500

    fix resumeview height

--- ResumeBuilder/install-dotnet.sh
+++ ResumeBuilder/install-dotnet.sh
@@ -6,8 +6,5 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
 # Add .NET to the PATH
 export PATH="$HOME/.dotnet:$PATH"
 
-# Verify installation
-dotnet --version
-
 # Publish the application
 dotnet publish -c Release -o ./publish
